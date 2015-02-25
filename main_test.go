0669a4866a872229dd211867fd4ce653e64d8776
Ryan Taylor Long <ryan@rtlong.com>
2015-02-24 19:33:44 -0800

    Add skeleton

---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	Convey("it's alive!!", t, func() {
+		So(1, ShouldEqual, 1)
+	})
+}
